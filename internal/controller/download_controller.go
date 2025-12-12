b8930b33e6a4772b5ff37f56cb8c0a87ccda256f
Yunus Kaya <yunusekaya2008@gmail.com>
2025-12-12 23:16:21 +0100

    update comment on reconcile func

--- internal/controller/download_controller.go
+++ internal/controller/download_controller.go
@@ -178,13 +178,6 @@ type DownloadReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Download object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.22.4/pkg/reconcile
 func (r *DownloadReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := ctrl.Log.WithValues("Download", req.NamespacedName)
 
